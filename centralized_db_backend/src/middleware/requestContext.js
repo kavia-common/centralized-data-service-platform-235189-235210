146dfeebf3f05b8c4abb5cdaf3bdb07968691dd1
Kavia AI <production@kavia.ai>
2026-03-03 05:21:11 +0000

    Checkpoint 6 - cg1d7025a8

--- centralized_db_backend/src/middleware/requestContext.js
+++ centralized_db_backend/src/middleware/requestContext.js
@@ -3,13 +3,15 @@
 const crypto = require('crypto');
 
 // PUBLIC_INTERFACE
-function requestContext(req, res, next) {
-  /** Attach stable request ID to every request. */
-  const incoming = req.get('x-request-id');
-  const requestId = incoming && incoming.length <= 128 ? incoming : crypto.randomUUID();
-  req.requestId = requestId;
-  res.setHeader('x-request-id', requestId);
-  next();
+function requestContext() {
+  /** Create middleware that attaches a stable request ID to every request. */
+  return (req, res, next) => {
+    const incoming = req.get('x-request-id');
+    const requestId = incoming && incoming.length <= 128 ? incoming : crypto.randomUUID();
+    req.requestId = requestId;
+    res.setHeader('x-request-id', requestId);
+    next();
+  };
 }
 
 module.exports = requestContext;
